838205a4d33022807ab6bae7b6feb1802c045c64
ANDI ARDIANSYAH NASIR <222111890@stis.ac.id>
2024-11-24 10:25:36 +0700

    first commit

---
+++ world_tour/asiatouragency/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+# urlpatterns = [
+#     path('', views.index)
+# ]
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('contact/', views.contact_view, name='contact'),
+    path('contact/success', views.contact_success_view, name='contact-success'),   
+]
